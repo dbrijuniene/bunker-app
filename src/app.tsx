e1b1fbbee77794b57a73152e73de6ed218258fbd
dbrijuniene <liutkute.deimante@gmail.com>
2022-06-11 18:52:10 +0300

    restricted access when not signed in

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Main from './pages/main';
 import Items from './pages/items';
 import Places from './pages/places';
 import Dashboard from './pages/dashboard';
 import LoginPage from './pages/login';
 import RegisterPage from './pages/registration';
+import { useRootSelector } from './store/hooks';
 
-const App: React.FC = () => (
-  <Routes>
-    <Route path="/" element={<Main />}>
-      <Route index element={<LoginPage />} />
-      <Route path="registration" element={<RegisterPage />} />
-      <Route path="dashboard" element={<Dashboard />} />
-      <Route path="items" element={<Items />} />
-      <Route path="places" element={<Places />} />
-    </Route>
-  </Routes>
-);
+const App: React.FC = () => {
+  const user = useRootSelector((state) => state.shared.user);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Main />}>
+        <Route index element={user ? <Navigate to="/dashboard" /> : <LoginPage />} />
+        <Route path="registration" element={user ? <Navigate to="/dashboard" /> : <RegisterPage />} />
+        <Route path="dashboard" element={user ? <Dashboard /> : <Navigate to="/" />} />
+        <Route path="items" element={user ? <Items /> : <Navigate to="/" />} />
+        <Route path="places" element={user ? <Places /> : <Navigate to="/" />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
